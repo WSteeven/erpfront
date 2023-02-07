1f27fe26a9505fdfa0e8b09f25313cb397083701
WSteeven <wilson972906@gmail.com>
2023-02-07 17:03:27 -0500

    correccion del merge con unidad de medida

--- src/pages/bodega/unidades_medidas/infraestructure/UnidadMedidaController.ts
+++ src/pages/bodega/unidades_medidas/infraestructure/UnidadMedidaController.ts
@@ -1,15 +1,4 @@
 import { TransaccionSimpleController } from "shared/contenedor/modules/simple/infraestructure/TransacccionSimpleController";
-<<<<<<< HEAD
-import { UnidadMedida} from "../domain/UnidadMedida";
-import { endpoints } from "config/api";
-
-export class UnidadMedidaController extends TransaccionSimpleController<UnidadMedida>{
-    constructor(){
-        super(endpoints.unidades_medidas)
-    }
-}
-
-=======
 import { UnidadMedida } from "../domain/UnidadMedida";
 import { endpoints } from "config/api";
 
@@ -20,4 +9,3 @@ export class UnidadMedidaController extends TransaccionSimpleController<UnidadMe
 }
 
 
->>>>>>> 523df83a27c9d3cf84816a0d2d36988a6d032620
