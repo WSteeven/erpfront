cd995f43a39e77a62c84b63e6849aeef7769e092
henvisi-1994 <henvisi_1994@hotmail.com>
2023-02-28 12:00:05 -0500

    Recuperacion de Contraseña

--- src/pages/sistema/authentication/forgotPassword/view/forgotPassword.page.ts
+++ src/pages/sistema/authentication/forgotPassword/view/forgotPassword.page.ts
@@ -1,52 +1,83 @@
 // Dependencias
 import { computed, defineComponent, reactive, ref } from 'vue'
+import { Vue3Lottie } from 'vue3-lottie'
+import 'vue3-lottie/dist/style.css'
 // Componentes
-import { Notificaciones } from '@/app/shared/componentes/toastification/application/notificaciones'
 // Logica y controladores
-import { LoginUser } from '@sistema/authentication/login/domain/loginUser.domain'
+import { StatusEssentialLoading } from 'components/loading/application/StatusEssentialLoading'
+import { useNotificaciones } from 'shared/notificaciones'
 import { ForgotPasswordController } from '../infraestructure/forgotPassword.controller'
-import { isAxiosError, notificarMensajesError } from '@/app/shared/utils'
+import { ForgotPassword } from '../domain/ForgotPassword'
+import { isAxiosError, notificarMensajesError } from 'shared/utils'
 
 export default defineComponent({
   name: 'ForgotPassword',
-  components: {},
+  components: {
+    LottiePlayer: Vue3Lottie,
+  },
   setup() {
-    const loginUser = reactive(new LoginUser())
+    const forgotPassword = reactive(new ForgotPassword())
     const enviando = ref(false)
 
     const forgotPasswordController = new ForgotPasswordController()
+    const notificaciones = useNotificaciones()
+    const cargando = new StatusEssentialLoading()
 
-    const notificaciones = new Notificaciones()
-    const enviarCorreoRecuperacion = () => {
+    const enviarCorreoRecuperacion = async () => {
       enviando.value = true
-      forgotPasswordController
-        .enviarCorreoRecuperacion(loginUser)
-        .then(() => {
-          notificaciones.notificarCorrecto(
-            'El correo de recuperación se ha enviado con éxito!'
-          )
-          loginUser.email = null
-        })
-        .catch((error) => {
-          if (isAxiosError(error)) {
-            const mensajes: string[] = error.erroresValidacion
-            notificarMensajesError(mensajes)
-          }
-        })
-        .finally(() => (enviando.value = false))
-    }
+      try {
 
+        cargando.activar()
+        await forgotPasswordController.enviarCorreoRecuperacion(forgotPassword)
+        notificaciones.notificarAdvertencia('Porfavor revise su codigo de confirmacion en su  Correo Institucional')
+
+      } catch (error: any) {
+        if (isAxiosError(error)) {
+          const mensajes: string[] = error.erroresValidacion
+          notificarMensajesError(mensajes, notificaciones)
+        }
+      } finally {
+        cargando.desactivar()
+        enviando.value = true
+      }
+    }
+    const recuperacionCuenta = async () => {
+      enviando.value = true
+      try {
+        cargando.activar()
+        await forgotPasswordController.recuperacionCuenta(forgotPassword)
+        notificaciones.notificarAdvertencia('Porfavor revise su codigo de confirmacion en su  Correo Institucional')
+
+      } catch (error: any) {
+        if (isAxiosError(error)) {
+          const mensajes: string[] = error.erroresValidacion
+          notificarMensajesError(mensajes, notificaciones)
+        }
+      } finally {
+        cargando.desactivar()
+        enviando.value = false
+      }
+    }
     const enableLoginButton = computed(
-      () => loginUser.email !== '' && loginUser.password !== ''
+      () =>
+      forgotPassword.email !== ''
+    )
+    const enableRecoveryPasswordButton = computed(
+      () =>
+      forgotPassword.password !== '' &&
+      forgotPassword.password === forgotPassword.password_confirmation
     )
 
     return {
-      loginUser,
+      forgotPassword,
+      isPwd: ref(true),
       enviando,
       // computed
       enableLoginButton,
+      enableRecoveryPasswordButton,
       // functions
       enviarCorreoRecuperacion,
+      recuperacionCuenta
     }
   },
 })
