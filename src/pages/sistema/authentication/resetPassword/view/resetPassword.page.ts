db8c6dbebe6e25f9ef1119bf7a63ecd15eaea1ea
henvisi-1994 <henvisi_1994@hotmail.com>
2023-02-28 17:19:39 -0500

    Redireccion a login desde reseteo de password

--- src/pages/sistema/authentication/resetPassword/view/resetPassword.page.ts
+++ src/pages/sistema/authentication/resetPassword/view/resetPassword.page.ts
@@ -9,18 +9,23 @@ import { ResetPassword} from '../domain/ResetPassword'
 import { ResetPasswordController } from '../infraestructure/resetPassword.controller'
 import { isAxiosError, notificarMensajesError } from 'shared/utils'
 import { useAuthenticationStore } from 'stores/authentication'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
   name: 'ResetPassword',
   components: {  LottiePlayer: Vue3Lottie },
   setup() {
     const resetPassword = reactive(new ResetPassword())
+    const Router = useRouter()
     const enviando = ref(false)
     const store = useAuthenticationStore();
     const resetPasswordController = new ResetPasswordController()
     const notificaciones = useNotificaciones()
     const cargando = new StatusEssentialLoading()
     resetPassword.nombreUsuario = store.getNombreusuario();
+    if (resetPassword.nombreUsuario === undefined) {
+      Router.replace('/login')
+    }
     async function resetearPassword() {
       enviando.value = true
       try {
