5b41efe1d559d71b85eebd2c051745d9038a3ce0
henvisi-1994 <henvisi_1994@hotmail.com>
2023-04-24 17:38:30 -0500

    correcciones de formulario de gastos

--- src/pages/fondosRotativos/gasto/view/GastoPage.vue
+++ src/pages/fondosRotativos/gasto/view/GastoPage.vue
@@ -126,8 +126,8 @@
           <!-- Factura -->
           <div class="col-12 col-md-3" v-if="esFactura">
             <label class="q-mb-sm block">#Factura</label>
-            <q-input v-model="gasto.factura" placeholder="Obligatorio" mask="###-###-#############" fill-mask
-              hint="###-###-#############" :disable="disabled" :error="!!v$.factura.$errors.length"
+            <q-input v-model="gasto.factura" placeholder="Obligatorio" :mask="mascaraFactura" fill-mask
+              :hint="mascaraFactura" :disable="disabled" :error="!!v$.factura.$errors.length"
               @blur="v$.factura.$touch" outlined dense>
               <template v-slot:error>
                 <div v-for="error of v$.factura.$errors" :key="error.$uid">
