6c46c94f5e8968e6f8b31f3dc70666e05f9ae821
WSteeven <wilson972906@gmail.com>
2023-06-28 18:54:58 -0500

    correcciones en criterios de calificacion

--- src/pages/comprasProveedores/criteriosCalificaciones/view/CriterioCalificacionPage.ts
+++ src/pages/comprasProveedores/criteriosCalificaciones/view/CriterioCalificacionPage.ts
@@ -4,11 +4,6 @@ import { required } from "shared/i18n-validators";
 
 //Componentes
 import TabLayout from "shared/contenedor/modules/simple/view/TabLayout.vue";
-import TablaFilasDinamicas from 'components/tables/view/TablaFilasDinamicas.vue'
-import EssentialTable from "components/tables/view/EssentialTable.vue";
-
-
-//Logica y Controladores
 import { Ref, defineComponent, ref } from "vue";
 import { ContenedorSimpleMixin } from "shared/contenedor/modules/simple/application/ContenedorSimpleMixin";
 import { CriterioCalificacion } from "../domain/CriterioCalificacion";
@@ -18,78 +13,54 @@ import { OfertaProveedorController } from "sistema/proveedores/modules/ofertas_p
 import { DepartamentoController } from "pages/recursosHumanos/departamentos/infraestructure/DepartamentoController";
 import { Departamento } from "pages/recursosHumanos/departamentos/domain/Departamento";
 import useVuelidate from "@vuelidate/core";
-import { CustomActionTable } from "components/tables/domain/CustomActionTable";
 
+//Logica y Controladores
 
 
 export default defineComponent({
-  components: { TabLayout, TablaFilasDinamicas, EssentialTable },
-  setup() {
-    const mixin = new ContenedorSimpleMixin(CriterioCalificacion, new CriterioCalificacionController())
-    const { entidad: criterio, disabled, accion, listadosAuxiliares, listado: criteriosRealizados } = mixin.useReferencias()
-    const { guardar: guardarCriterio, setValidador, cargarVista, obtenerListados } = mixin.useComportamiento()
-    const { onConsultado } = mixin.useHooks()
+    components: { TabLayout },
+    setup() {
+        const mixin = new ContenedorSimpleMixin(CriterioCalificacion, new CriterioCalificacionController())
+        const { entidad: criterio, disabled, accion, listadosAuxiliares } = mixin.useReferencias()
+        const { setValidador, cargarVista, obtenerListados } = mixin.useComportamiento()
+        const { onConsultado } = mixin.useHooks()
 
-    const departamento = ref()
-    const store = useAuthenticationStore()
-    const ofertas = ref([])
+        const departamento = ref()
+        const store = useAuthenticationStore()
+        const ofertas = ref([])
 
-    cargarVista(async () => {
-      obtenerListados({
-        ofertas: new OfertaProveedorController(),
-      })
-      const { result } = await new DepartamentoController().listar({ responsable_id: store.user.id })
-      departamento.value = result
-    })
-    /**************************************************************
-    * Validaciones
-    **************************************************************/
-    const reglas = {
-      nombre: { required },
-      descripcion: { required },
-      ponderacion_referencia: { required },
-      oferta: { required },
+        cargarVista(async () => {
+            obtenerListados({
+                ofertas: new OfertaProveedorController(),
+            })
+            const {result} = await new DepartamentoController().listar({ responsable_id: store.user.id })
+            departamento.value = result
+        })
+        /**************************************************************
+        * Validaciones
+        **************************************************************/
+        const reglas = {
+            nombre: {required},
+            descripcion: {required},
+            ponderacion_referencia: {required},
+            oferta: {required},
+        }
+        const v$ = useVuelidate(reglas, criterio)
+        setValidador(v$.value)
+
+        //llenar listados
+        ofertas.value = listadosAuxiliares.ofertas
+
+        return {
+            mixin, criterio, disabled, v$, accion,
+            configuracionColumnas: configuracionColumnasCriteriosCalificaciones,
+
+            store, //store de usuario autenticado
+            //listados
+            ofertas,
+            departamento,
+
+        }
     }
-    const v$ = useVuelidate(reglas, criterio)
-    setValidador(v$.value)
-
-    /**************************************************************
-    * Funciones
-    **************************************************************/
-    const agregarCriterioBienes: CustomActionTable = {
-      titulo: "Agregar Criterio para calificar Bienes",
-      tooltip: "Agregar Criterio Bienes",
-      icono: 'bi-person-fill-add',
-      color: 'positive',
-      accion: (param) => {
-        console.log(param);
-      }
-    }
-    function guardarFilaCriterio(data) {
-      criterio.hydrate(data)
-      guardarCriterio(data)
-    }
-
-    //llenar listados
-    ofertas.value = listadosAuxiliares.ofertas
-
-    const columnas = [...configuracionColumnasCriteriosCalificaciones]
-
-    return {
-      mixin, criterio, disabled, v$, accion,
-      configuracionColumnas: configuracionColumnasCriteriosCalificaciones,
-      columnas,
-
-      store, //store de usuario autenticado
-      //listados
-      ofertas,
-      departamento,
-      guardarFilaCriterio,
-      criteriosRealizados,
-      agregarCriterioBienes,
-      CriterioCalificacion,
-
-    }
-  }
 })
 
