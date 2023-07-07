af00b6162ec72bfa2d46e3a6285ee460243cc2dc
juancuestav <juanbryanc@gmail.com>
2023-07-06 22:26:22 -0500

    Colores reporte tareas

--- src/components/loading/application/StatusEssentialLoading.ts
+++ src/components/loading/application/StatusEssentialLoading.ts
@@ -18,7 +18,7 @@ export class StatusEssentialLoading {
       spinner: QSpinnerPie,
       message: 'La operación está en progreso.<br/><span class="text-blue-grey-2 text-italic">Espere por favor...</span>',
       html: true,
-      spinnerColor: 'white',
+      spinnerColor: 'blue-grey-3',
     })
   }
 
