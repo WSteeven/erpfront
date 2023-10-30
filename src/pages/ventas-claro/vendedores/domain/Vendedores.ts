20b2641c921c8a38e08427b4b730f73b95a1950f
henvisi-1994 <henvisi_1994@hotmail.com>
2023-10-30 16:28:39 -0500

    fix:modelo Vendedores

--- src/pages/ventas-claro/vendedores/domain/Vendedores.ts
+++ src/pages/ventas-claro/vendedores/domain/Vendedores.ts
@@ -1,17 +1,23 @@
 import { EntidadAuditable } from 'shared/entidad/domain/entidadAuditable'
 export class Vendedores extends EntidadAuditable {
+  id: number | null;
   codigo_vendedor: string | null
   empleado:number | null
+  empleado_id: number | null
   empleado_info: string | null
   modalidad:number | null
+  modalidad_id: number | null
   modalidad_info: string | null
 
   constructor() {
     super()
+    this.id = null
     this.codigo_vendedor = null
     this.empleado = null
+    this.empleado_id = null
     this.empleado_info = null
     this.modalidad = null
+    this.modalidad_id =null
     this.modalidad_info = null
   }
 }
