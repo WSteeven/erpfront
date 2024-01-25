96c1cba4f0a90313f76c25f58e45ebd8e392a104
juancuestav <juanbryanc@gmail.com>
2024-01-25 16:55:01 -0500

    Medico

--- src/pages/medico/gestionarPacientes/view/GestionarPacientePage.vue
+++ src/pages/medico/gestionarPacientes/view/GestionarPacientePage.vue
@@ -40,9 +40,9 @@
           no-caps
         />
         <q-tab
-          name="3"
+          name="4"
           label="Salida"
-          :class="{ 'tab-inactive': tabs !== '3' }"
+          :class="{ 'tab-inactive': tabs !== '4' }"
           no-caps
         />
       </q-tabs>
@@ -57,11 +57,16 @@
         :class="{ 'rounded-tabpanel': !$q.screen.xs }"
       >
         <q-tab-panel name="1">
+          <div class="row justify-end">
+            <q-btn color="primary" no-caps rounded
+              >Agregar un registro de examen de ingreso</q-btn
+            >
+          </div>
           <q-splitter v-model="splitterModel" style="height: 250px">
             <template v-slot:before>
               <!-- Tabs -->
               <q-tabs
-                v-model="tabs"
+                v-model="tabsRegistro"
                 vertical
                 switch-indicator
                 active-class="bg-primary text-white"
@@ -79,34 +84,54 @@
                   :class="{ 'tab-inactive': tabs !== '2' }"
                   no-caps
                 />
-                <q-tab
-                  name="3"
-                  label="Registro #3"
-                  :class="{ 'tab-inactive': tabs !== '3' }"
-                  no-caps
-                />
-                <q-tab
-                  name="4"
-                  label="Registro #4"
-                  :class="{ 'tab-inactive': tabs !== '3' }"
-                  no-caps
-                />
               </q-tabs>
             </template>
 
             <template v-slot:after>
               <q-tab-panels
-                v-model="tabs"
+                v-model="tabsRegistro"
                 animated
                 transition-prev="scale"
                 transition-next="scale"
                 helpalive
                 :class="{ 'rounded-tabpanel': !$q.screen.xs }"
               >
-                <q-tab-panel name="1"> Ingreso </q-tab-panel>
+                <q-tab-panel name="1">
+                  <div class="text-primary text-bold">Exámenes comunes</div>
+                  <div class="row text-bold text-center">
+                    <div class="col-12 col-md-4">TIPO DE EXAMEN</div>
+                    <div
+                      v-for="estado in estadosExamenes"
+                      :key="estado.id"
+                      class="col-12 col-md-2"
+                    >
+                      {{ estado.nombre }}
+                    </div>
+                  </div>
+
+                  <!-- Categoria -->
+                  <div
+                    v-for="registro in registroExamenes"
+                    :key="registro.id"
+                    class="row"
+                  >
+                    <div class="text-bold">{{ registro.categoria }}</div>
+                    <!-- <div>{{ registro.examenes }}</div> -->
+                    <div
+                      v-for="examen in registro.examenes"
+                      :key="examen.examen"
+                      class=""
+                    >
+                      <q-checkbox
+                        v-model="examen.seleccionado"
+                        :label="examen.examen"
+                        outlined
+                        dense
+                      ></q-checkbox>
+                    </div>
+                  </div>
+                </q-tab-panel>
                 <q-tab-panel name="2"> Ocupacionales </q-tab-panel>
-                <q-tab-panel name="3"> Reingreso </q-tab-panel>
-                <q-tab-panel name="4"> Salida </q-tab-panel>
               </q-tab-panels>
             </template>
           </q-splitter>
