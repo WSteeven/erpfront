31566ee85d718f07d2739069be59c359fc46c557
juancuestav <juanbryanc@gmail.com>
2024-03-07 08:25:11 -0500

    Tareas: Mostrar tiempo ocupado en Proyectos.

--- src/shared/entidad/domain/entidadAuditable.ts
+++ src/shared/entidad/domain/entidadAuditable.ts
@@ -2,6 +2,9 @@ import { Hidratable } from './Hidratable'
 
 export class EntidadAuditable extends Hidratable { //implements Auditable {
   id: number | null
+  created_at: string | null
+  updated_at: string | null
+
   // negocio: number | null
   // usuario_inserta: number | null
   // usuario_modifica: number | null
@@ -11,6 +14,8 @@ export class EntidadAuditable extends Hidratable { //implements Auditable {
   constructor() {
     super()
     this.id = null
+    this.created_at = null
+    this.updated_at = null
     // this.negocio = null
     // this.usuario_inserta = null
     // this.usuario_modifica = null
