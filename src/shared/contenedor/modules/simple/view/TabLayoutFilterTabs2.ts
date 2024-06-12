b71102636fa99bfde4b27e48352be951d0da8d95
antonnick28 <ecanarte28@gmail.com>
2024-06-12 09:37:56 -0500

    cambios

--- src/shared/contenedor/modules/simple/view/TabLayoutFilterTabs2.ts
+++ src/shared/contenedor/modules/simple/view/TabLayoutFilterTabs2.ts
@@ -43,7 +43,7 @@ export default defineComponent({
       type: Boolean,
       default: true,
     },
-    
+
     permitirConsultar: {
       type: Boolean,
       default: true,
@@ -269,10 +269,8 @@ export default defineComponent({
       accionPersonalizada,
       esBodeguero,
       esCoordinador,
-      forzarListar,
       // Valor del essentialLoading
       storeCargando: useCargandoStore(),
-      puedeExportar: props.puedeExportar,
       // custom tab panel
       ejecutarUnaVezCustomPanel1,
       ejecutarUnaVezCustomPanel2,
