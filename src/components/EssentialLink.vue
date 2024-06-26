2ec159b0968b5bcb13bb1f59296bb3a28b84817e
juancuestav <juanbryanc@gmail.com>
2024-06-26 15:45:39 -0500

    Tareas: Reporte de materiales utilizados finalizado

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -37,7 +37,7 @@
           :icon="child.icon"
           :children="child.children"
           :can="child.can"
-          :class="{'bg-white border-white rounded q-mb-xs': !!child.children}"
+          :class="{'bg-desenfoque border-white rounded q-mb-xs': !!child.children}"
         ></EssentialLink>
       </div>
     </q-expansion-item>
