bee96e04e7e61b165b5220824fa71d7f499a11d9
WSteeven <wilson972906@gmail.com>
2024-07-24 09:46:02 -0500

    modificando VisualizarVacantePage

--- src/pages/recursosHumanos/SeleccionContratacionPersonal/vacantes/view/VisualizarVacantePage.vue
+++ src/pages/recursosHumanos/SeleccionContratacionPersonal/vacantes/view/VisualizarVacantePage.vue
@@ -1,9 +1,22 @@
 <template>
   <basic-container>
-    <q-card-section>
-      <div>Visualización de la vacante</div>
-      {{ vacante }}
-    </q-card-section>
+    <template #contenido>
+      <q-card-section>
+        <div>Visualización de la vacante</div>
+        {{ vacante }}
+      </q-card-section>
+      <q-separator></q-separator>
+      <q-card-section>
+        <div>hhhhh</div>
+        <div>aaaaa</div>
+        <div>ttttt</div>
+        <div>ggggg</div>
+        <div>lllll</div>
+        <div>ccccc</div>
+        <div>mmmmm</div>
+        <div>yyyyy</div>
+      </q-card-section>
+    </template>
   </basic-container>
 </template>
 
--- src/shared/contenedor/modules/basic/view/BasicContainer.vue
+++ src/shared/contenedor/modules/basic/view/BasicContainer.vue
@@ -1,12 +1,12 @@
 <template>
   <q-page :padding="!$q.screen.xs">
-    <q-card class="rounded bg-desenfoque border-blue custom-shadow">
+    <transition name="scale" mode="out-in">
       <slot name="modales" />
+    </transition>
+    <q-card class="rounded bg-desenfoque border-blue custom-shadow">
       <slot name="contenido" />
     </q-card>
   </q-page>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
