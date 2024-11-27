39366de6e7ae0839f95dfc29e117f321979caac6
juancuestav <juanbryanc@gmail.com>
2024-11-27 11:29:20 -0500

    Resolucion de conflictos en merge de stash

--- src/stores/menu.ts
+++ src/stores/menu.ts
@@ -767,6 +767,7 @@ export const useMenuStore = defineStore('menu', () => {
               link: 'solicitudes-vacaciones',
               icon: 'bi-umbrella-fill',
               can: store.can('puede.acceder.solicitudes_vacaciones')
+<<<<<<< Updated upstream
             },
             // {
             //   title: 'Planes de Vacaciones',
@@ -775,6 +776,8 @@ export const useMenuStore = defineStore('menu', () => {
             //   can: store.can('puede.acceder.planes_vacaciones')
             // },
             {
+=======
+>>>>>>> Stashed changes
             }, {
               title: 'Registro de Vacaciones',
               link: 'vacaciones',
@@ -894,7 +897,11 @@ export const useMenuStore = defineStore('menu', () => {
               link: 'reporte-vacaciones',
               icon: 'bi-sunglasses',
               can: store.can('puede.acceder.reportes_rrhh') || true
+<<<<<<< Updated upstream
             }
+=======
+            },
+>>>>>>> Stashed changes
           ]
         }
       ]
