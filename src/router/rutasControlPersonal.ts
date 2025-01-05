40bf69fe3f54ae35d569ec64174d7910c02abdf6
antonnick28 <ca.arte07@hotmail.com>
2025-01-05 17:57:20 -0500

    modificacion de las rutasControlPersonal

--- src/router/rutasControlPersonal.ts
+++ src/router/rutasControlPersonal.ts
@@ -1,51 +1,36 @@
-import { RouteRecordRaw } from 'vue-router';
+import { RouteRecordRaw } from 'vue-router'
 
 const rutasControlPersonal: RouteRecordRaw[] = [
   {
     path: '/asistencia',
     name: 'asistencia',
-    component: () =>
-      import(
-        'controlPersonal/asistencia/view/AsistenciaPage.vue'
-      ),
-    meta: { requiresAuth: true },
+    component: () => import('src/pages/recursosHumanos/control_personal/asistencia/view/AsistenciaPage.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/dashboard-control-personal',
     name: 'dashboard_control_personal',
-    component: () =>
-      import(
-        'controlPersonal/dashboard_control_personal/view/DashboardControlPersonal.vue'
-      ),
-    meta: { requiresAuth: true },
+    component: () => import('src/pages/recursosHumanos/control_personal/dashboard_control_personal/view/DashboardControlPersonal.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/atrasos',
     name: 'atrasos',
-    component: () =>
-      import(
-        'controlPersonal/justificaciones_asistencia/view/JustificacionPage.vue'
-      ),
-    meta: { requiresAuth: true },
+    component: () => import('src/pages/recursosHumanos/control_personal/justificaciones_asistencia/view/JustificacionPage.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/horario-laboral',
     name: 'horario_laboral',
-    component: () =>
-      import(
-        'controlPersonal/horario_laboral/view/HorarioLaboralPage.vue'
-      ),
-    meta: { requiresAuth: true },
+    component: () => import('src/pages/recursosHumanos/control_personal/horario_laboral/view/HorarioLaboralPage.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/horario-almuerzo',
     name: 'horario_almuerzo',
-    component: () =>
-      import(
-        'controlPersonal/horario_almuerzo/view/HorarioDeAlmuerzoPage.vue'
-      ),
-    meta: { requiresAuth: true },
-  },
-];
+    component: () => import('src/pages/recursosHumanos/control_personal/horario_almuerzo/view/HorarioDeAlmuerzoPage.vue'),
+    meta: { requiresAuth: true }
+  }
+]
 
-export default rutasControlPersonal;
+export default rutasControlPersonal
