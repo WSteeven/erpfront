07026d3a13d63379f92cd691784d0db3558a12d2
antonnick28 <ca.arte07@hotmail.com>
2025-02-26 12:05:31 -0500

    Actualización del Login - Regreso al Login Anterior

--- src/pages/sistema/authentication/login/view/LoginPage.vue
+++ src/pages/sistema/authentication/login/view/LoginPage.vue
@@ -1,216 +1,147 @@
 <template>
   <q-page>
-    <div class="login-container">
-      <div class="login-card">
-
-        <!-- Bienvenida -->
-        <span class="welcome-text">Bienvenidos a</span>
-        <div class="logo-container">
-          <q-avatar square style="height: 200px; width: auto;">
+    <div class="row items-center">
+      <!-- Left side -->
+      <div
+        v-if="!$q.screen.xs && !$q.screen.sm"
+        class="col-12 col-md-8 justify-center q-pa-lg items-center row window-height"
+        :class="{ 'bg-grey-2': !$q.dark.isActive }"
+      >
+        <div class="imagen d-flex align-items-center justify-content-center">
+          <q-avatar square size="400px">
             <img :src="!$q.dark.isActive ? logoClaro : logoOscuro" />
           </q-avatar>
         </div>
+      </div>
 
-        <!-- Formulario de inicio de sesión -->
-        <form @submit.prevent="login" class="login-form" @keyup.enter="login">
-          <div class="input-wrapper">
-            <q-icon name="mail" class="input-icon" />
+      <!-- Right side -->
+      <div
+        class="col-12 col-md-4 column items-center bg-body-table justify-center window-height"
+      >
+        <q-avatar
+          v-if="$q.screen.xs"
+          square
+          size="120px"
+          class="q-mx-auto block q-mb-md"
+        >
+          <img :src="!$q.dark.isActive ? logoClaro : logoOscuro" />
+        </q-avatar>
+
+        <form @submit.prevent="login" class="full-width q-px-lg">
+          <div class="q-mb-sm">
+            <h2>Bienvenidos a {{ nombreEmpresa }}</h2>
+            <span>Inicie sesión con su cuenta</span>
+          </div>
+
+          <!-- Usuario -->
+          <div class="col-12 q-mb-sm">
             <q-input
               v-model="loginUser.name"
-              label="Correo electrónico"
+              label="Usuario"
               outlined
               dense
-              class="input-field"
+              @keyup.enter="login()"
             />
           </div>
 
-          <div class="input-wrapper">
-            <q-icon name="lock" class="input-icon" />
+          <!-- Contraseña -->
+          <div class="col-12 q-mb-sm">
             <q-input
               v-model="loginUser.password"
               label="Contraseña"
               outlined
               dense
               :type="isPwd ? 'password' : 'text'"
-              class="input-field"
+              class="normal-text"
+              hint="No comparta su contraseña con nadie"
+              @keyup.enter="login()"
+              style="text-transform: none;"
             >
               <template v-slot:append>
                 <q-icon
                   :name="isPwd ? 'visibility_off' : 'visibility'"
-                  class="toggle-icon"
-                  @click="togglePasswordVisibility"
+                  class="cursor-pointer"
+                  @click="isPwd = !isPwd"
                 />
               </template>
             </q-input>
           </div>
 
-          <q-btn
-            label="Iniciar sesión"
-            class="btn-login"
-            :disabled="!enableLoginButton"
-            no-caps
-            unelevated
-            @click="login"
-          />
-          <span class="forgot-text" @click="recuperarPassword">
-            ¿Olvidó su nombre de usuario o contraseña?
-          </span>
+          <!-- Recuerdame -->
+          <div class="col-12 q-mb-sm">
+            <q-toggle v-model="loginUser.remember_session" label="Recuérdame" />
+          </div>
+
+          <div class="col-12">
+            <!-- Botones -->
+            <q-btn
+              color="primary"
+              label="Iniciar sesión"
+              class="full-width q-mb-sm"
+              :disabled="!enableLoginButton"
+              no-caps
+              unelevated
+              @click="login()"
+            >
+            </q-btn>
+          </div>
+          <div class="col-12">
+            <!-- Botones -->
+            <q-btn
+              flat
+              color="primary"
+              label="Recuperar contraseña"
+              class="full-width q-mb-sm"
+              no-caps
+              unelevated
+              @click="recuperarPassword()"
+            >
+            </q-btn>
+          </div>
+
+          <div class="col-12 q-pt-xl q-mt-xl">
+            <q-btn
+              color="primary"
+              label="¿Entrar como externo?"
+              class="full-width q-mb-sm"
+              no-caps
+              unelevated
+
+              outline
+              :to="{name:'LoginPostulante'}"
+            />
+          </div>
         </form>
       </div>
     </div>
   </q-page>
 </template>
 
-
 <script src="./LoginPage.ts"></script>
 
-<style scoped>
-/* Contenedor principal */
-.login-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: var(--q-color-grey-2);
-  padding: 1rem;
+<style scope>
+h2 {
+  line-height: 1.2;
+  font-size: 1.714rem;
 }
 
-/* Tarjeta de inicio de sesión */
-.login-card {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border-radius: 16px;
-  padding: 2.5rem;
-  max-width: 480px; /* Card más grande */
-  width: 100%;
-  text-align: center;
+.empresa {
+  position: fixed;
+  top: 16px;
+  left: 16px;
 }
 
-/* Texto de bienvenida */
-.welcome-text {
-  font-size: 1.25rem;
-  font-weight: 500;
-  color: var(--q-color-grey-8);
-  margin-bottom: 1rem;
+.fondo {
+  background: rgb(94, 88, 252);
+  background: linear-gradient(
+    90deg,
+    rgba(94, 88, 252, 1) 0%,
+    rgba(110, 143, 255, 1) 100%
+  );
 }
 
-/* Nombre de la empresa */
-.company-name {
-  font-size: 1.75rem;
-  font-weight: 700;
-  color: var(--q-color-primary);
-  margin: 0.5rem 0 1.5rem;
-}
-
-/* Contenedor del logo */
-.logo-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 1.5rem;
-}
-
-/* Formulario */
-.login-form {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 1.5rem;
-}
-
-/* Input con iconos */
-.input-wrapper {
-  position: relative;
-  display: flex;
-  align-items: center;
-}
-
-.input-icon {
-  position: absolute;
-  left: 12px;
-  color: var(--q-color-grey-6);
-  font-size: 1.25rem;
-}
-
-.input-field {
-  width: 100%;
-  padding-left: 40px; /* Espacio para el icono */
-}
-
-/* Botón de inicio de sesión */
-.btn-login {
-  width: 100%;
-  padding: 0.75rem 0;
-  font-size: 1rem;
-  font-weight: 600;
-  background-color: rgb(6, 38, 97);
-  color: #ffffff;
-  border-radius: 8px;
-}
-
-/* Texto de recuperación de contraseña */
-.forgot-text {
-  font-size: 0.875rem;
-  color: var(--q-color-grey-7);
-  text-align: center;
-  margin-top: 1rem;
-  cursor: pointer;
-  text-decoration: underline;
-}
-.forgot-text:hover {
-  color: var(--q-color-grey-9);
-}
-
-
-/* Input general */
-.input-field {
-  width: 100%;
-  border-radius: 8px;
-  padding-left: 40px; /* Espacio para el icono */
-  font-size: 1rem;
-}
-
-/* Estilo mejorado */
-.improved-input {
-  background-color: var(--q-color-grey-1);
-  border: 1px solid var(--q-color-grey-4);
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
-  transition: all 0.2s ease-in-out;
-}
-
-/* Hover y foco */
-.improved-input:hover {
-  border-color: var(--q-color-primary);
-  background-color: var(--q-color-grey-2);
-}
-
-.improved-input:focus-within {
-  border-color: var(--q-color-primary);
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  background-color: #ffffff;
-}
-
-/* Iconos de input */
-.input-icon {
-  position: absolute;
-  left: 12px;
-  color: var(--q-color-grey-6);
-  font-size: 1.5rem;
-  transition: color 0.2s ease-in-out;
-}
-
-.input-wrapper:hover .input-icon {
-  color: var(--q-color-primary);
-}
-
-.toggle-icon {
-  color: var(--q-color-grey-6);
-  cursor: pointer;
-}
-
-.toggle-icon:hover {
-  color: var(--q-color-primary);
-}
+/* Quita las mayusculas al campo de contraseña y usuario */
+/* .q-field .q-field__inner {
+  text-transform: none !important;
+} */
 </style>
