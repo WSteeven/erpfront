a8d6a224ed267df402fed9605329400c1183e747
WSteeven <wilson972906@gmail.com>
2025-04-02 10:25:53 -0500

    feat: funcionalidad para TCKT-10324

---
+++ src/pages/fondosRotativos/gasto/modules/gastosRechazadosSistema/view/GastoRechazadoPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page padding>
+    <div class="col-12 row justify-end q-gutter-sm q-mb-md">
+      <q-btn color="primary" no-caps @click="actualizarListado">
+        <q-icon name="bi-arrow-clockwise" size="xs" class="q-pr-sm" />
+        <span>Actualizar</span>
+      </q-btn>
+    </div>
+    <essential-table
+      :datos="listado"
+      :configuracion-columnas="configuracionColumnas"
+      ajustar-celdas
+      :permitirConsultar="false"
+      :permitirEditar="false"
+      :permitirEliminar="false"
+      :permitirBuscar="true"
+      :accion1="btnReactivarGasto"
+    />
+  </q-page>
+</template>
+
+<script src="./GastoRechazadoPage.ts" />
